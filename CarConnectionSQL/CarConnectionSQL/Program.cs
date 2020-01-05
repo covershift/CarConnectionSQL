c55b65142b65535f6dff5e062bf2de7de03ddc69
Bruno Marquez M <bruno.marquez.m@outlook.com>
2020-01-05 01:00:44 -0500

    Creation of the project and base message to start process.

---
+++ CarConnectionSQL/CarConnectionSQL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarConnectionSQL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Press 'Enter' to begin process of transformation");
+
+            while (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                TransformInsert();
+            }
+        }
+
+
+        async static void TransformInsert()
+        {
+            
+        }
+    }
+}
